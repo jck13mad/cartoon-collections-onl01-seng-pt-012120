78726fc5eb9ec6dd24485baaa8d1e80ec4b8079c
Jack Madden <jck13mad@gmail.com>
2020-02-01 14:55:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(array)
   new_array = []
   i = 0
   while i < array.length
-    new_array << array[i].capitalize
+    new_array << array[i].capitalize + "!"
   end
 end
 
